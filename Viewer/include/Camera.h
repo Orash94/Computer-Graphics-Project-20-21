2b3574f34800d89cd771cf75044b9f74585886e5
Abed <id.abed@gmail.com>
2020-11-12 09:50:31 +0200

    implemented lookAt function in camera class

--- Viewer/include/Camera.h
+++ Viewer/include/Camera.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <glm/glm.hpp>
-
+#include "Utils.h"
 class Camera
 {
 public:
@@ -8,11 +8,12 @@ public:
 	virtual ~Camera();
 
 	void SetCameraLookAt(const glm::vec3& eye, const glm::vec3& at, const glm::vec3& up);
-
+	glm::fmat4x4 lookAt(glm::fvec3& eye, glm::fvec3& at, glm::fvec3& up);
 	const glm::mat4x4& GetProjectionTransformation() const;
 	const glm::mat4x4& GetViewTransformation() const;
 
 private:
 	glm::mat4x4 view_transformation_;
+	glm::mat4x4 view_transformation_inverse;
 	glm::mat4x4 projection_transformation_;
 };
